676df01bdd1f6b4e21ca23981c4a801122b51ba7
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-07 01:26:21 -0300

    feat: adiciona comando configurar

---
+++ src/main/java/com/sistemautil/ConfigurarCommand.java
@@ -0,0 +1,42 @@
+package com.sistemautil;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.Collections;
+import java.util.List;
+
+public final class ConfigurarCommand implements CommandExecutor, TabCompleter {
+    private final UtilidadesGui gui;
+
+    public ConfigurarCommand(UtilidadesGui gui) {
+        this.gui = gui;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(ChatColor.RED + "Este comando só pode ser usado por jogadores.");
+            return true;
+        }
+        if (!player.hasPermission("sistemautil.configurar")) {
+            player.sendMessage(ChatColor.RED + "Você não tem permissão para usar este comando.");
+            return true;
+        }
+        if (args.length != 0) {
+            player.sendMessage(ChatColor.YELLOW + "Uso: /" + label);
+            return true;
+        }
+        gui.open(player);
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return Collections.emptyList();
+    }
+}
