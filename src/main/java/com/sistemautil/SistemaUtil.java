b0e8fa04d6abf3b4e932d6bb52001eb9839599f8
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-06 23:55:05 -0300

    refactor: load separated utility configs

--- src/main/java/com/sistemautil/SistemaUtil.java
+++ src/main/java/com/sistemautil/SistemaUtil.java
@@ -1,20 +1,24 @@
 package com.sistemautil;
 
+import com.sistemautil.config.ConfigManager;
 import com.sistemautil.motd.MotdCommand;
 import com.sistemautil.motd.MotdListener;
 import com.sistemautil.motd.MotdPlugin;
 import com.sistemautil.motd.TPSMonitor;
 import com.sistemautil.tab.ServerTabManager;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SistemaUtil extends JavaPlugin {
     private final TPSMonitor tpsMonitor = new TPSMonitor();
+    private final ConfigManager configManager = new ConfigManager(this);
     private MotdPlugin motd;
     private ServerTabManager tabManager;
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
+        configManager.loadAll();
+
         motd = new MotdPlugin(this);
         motd.enable();
 
@@ -50,5 +54,23 @@ public final class SistemaUtil extends JavaPlugin {
 
     public ServerTabManager getTabManager() {
         return tabManager;
+    }
+
+    public FileConfiguration getMotdConfig() {
+        return configManager.motd();
+    }
+
+    public FileConfiguration getTabConfig() {
+        return configManager.tab();
+    }
+
+    public FileConfiguration getCorConfig() {
+        return configManager.cor();
+    }
+
+    public void reloadConfigs() {
+        configManager.reloadAll();
+        if (motd != null) motd.recarregar();
+        if (tabManager != null) tabManager.start();
     }
 }
