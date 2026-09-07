20de446822ae57193cd95fa3edf1bc35bc8c3067
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-07 20:19:51 -0300

    reforcar bloqueio de colisao entre jogadores

--- src/main/java/com/sistemautil/PlayerCollisionListener.java
+++ src/main/java/com/sistemautil/PlayerCollisionListener.java
@@ -2,13 +2,20 @@ package com.sistemautil;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public final class PlayerCollisionListener implements Listener {
 
-    @EventHandler
+    public void disableCollision(Player player) {
+        if (player != null && player.isOnline()) {
+            player.setCollidable(false);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().setCollidable(false);
+        disableCollision(event.getPlayer());
     }
 }
