15c71fc30bfc65b5777cc638ded2d9acc1f270e7
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-06 23:44:57 -0300

    feat: enable server TAB manager

--- src/main/java/com/sistemautil/SistemaUtil.java
+++ src/main/java/com/sistemautil/SistemaUtil.java
@@ -4,11 +4,13 @@ import com.sistemautil.motd.MotdCommand;
 import com.sistemautil.motd.MotdListener;
 import com.sistemautil.motd.MotdPlugin;
 import com.sistemautil.motd.TPSMonitor;
+import com.sistemautil.tab.ServerTabManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SistemaUtil extends JavaPlugin {
     private final TPSMonitor tpsMonitor = new TPSMonitor();
     private MotdPlugin motd;
+    private ServerTabManager tabManager;
 
     @Override
     public void onEnable() {
@@ -19,6 +21,9 @@ public final class SistemaUtil extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new MotdListener(this), this);
         getServer().getPluginManager().registerEvents(new ServerCommandGuardListener(), this);
         tpsMonitor.start(this);
+
+        tabManager = new ServerTabManager(this);
+        tabManager.start();
 
         MotdCommand motdCommand = new MotdCommand(this);
         if (getCommand("motdplus") != null) {
@@ -31,6 +36,7 @@ public final class SistemaUtil extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        if (tabManager != null) tabManager.stop();
         tpsMonitor.stop();
     }
 
@@ -40,5 +46,9 @@ public final class SistemaUtil extends JavaPlugin {
 
     public MotdPlugin getMotd() {
         return motd;
+    }
+
+    public ServerTabManager getTabManager() {
+        return tabManager;
     }
 }
