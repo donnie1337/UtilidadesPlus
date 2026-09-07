dc6690a361984dfb783c632c64be685f07ded8ba
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-07 01:27:02 -0300

    feat: registra menu configurar e notificacoes

--- src/main/java/com/sistemautil/SistemaUtil.java
+++ src/main/java/com/sistemautil/SistemaUtil.java
@@ -14,16 +14,22 @@ public final class SistemaUtil extends JavaPlugin {
     private final ConfigManager configManager = new ConfigManager(this);
     private MotdPlugin motd;
     private ServerTabManager tabManager;
+    private UtilidadesPreferences utilidadesPreferences;
 
     @Override
     public void onEnable() {
         configManager.loadAll();
+        utilidadesPreferences = new UtilidadesPreferences(this);
+        utilidadesPreferences.load();
 
         motd = new MotdPlugin(this);
         motd.enable();
 
         getServer().getPluginManager().registerEvents(new MotdListener(this), this);
         getServer().getPluginManager().registerEvents(new ServerCommandGuardListener(), this);
+        getServer().getPluginManager().registerEvents(new UtilidadesGui(this, utilidadesPreferences), this);
+        getServer().getPluginManager().registerEvents(
+                new JoinQuitNotificationListener(this, utilidadesPreferences), this);
         tpsMonitor.start(this);
 
         tabManager = new ServerTabManager(this);
@@ -35,41 +41,34 @@ public final class SistemaUtil extends JavaPlugin {
             getCommand("motdplus").setTabCompleter(motdCommand);
         }
 
+        if (getCommand("configurar") != null) {
+            ConfigurarCommand configurarCommand = new ConfigurarCommand(
+                    new UtilidadesGui(this, utilidadesPreferences));
+            getCommand("configurar").setExecutor(configurarCommand);
+            getCommand("configurar").setTabCompleter(configurarCommand);
+        }
+
         getLogger().info("SistemaUtil ativado com sucesso.");
     }
 
     @Override
     public void onDisable() {
         if (tabManager != null) tabManager.stop();
+        if (utilidadesPreferences != null) utilidadesPreferences.save();
         tpsMonitor.stop();
     }
 
-    public TPSMonitor getTpsMonitor() {
-        return tpsMonitor;
-    }
-
-    public MotdPlugin getMotd() {
-        return motd;
-    }
-
-    public ServerTabManager getTabManager() {
-        return tabManager;
-    }
-
-    public FileConfiguration getMotdConfig() {
-        return configManager.motd();
-    }
-
-    public FileConfiguration getTabConfig() {
-        return configManager.tab();
-    }
-
-    public FileConfiguration getCorConfig() {
-        return configManager.cor();
-    }
+    public TPSMonitor getTpsMonitor() { return tpsMonitor; }
+    public MotdPlugin getMotd() { return motd; }
+    public ServerTabManager getTabManager() { return tabManager; }
+    public FileConfiguration getMotdConfig() { return configManager.motd(); }
+    public FileConfiguration getTabConfig() { return configManager.tab(); }
+    public FileConfiguration getCorConfig() { return configManager.cor(); }
+    public FileConfiguration getUtilidadesConfig() { return configManager.utilidades(); }
 
     public void reloadConfigs() {
         configManager.reloadAll();
+        if (utilidadesPreferences != null) utilidadesPreferences.load();
         if (motd != null) motd.recarregar();
         if (tabManager != null) tabManager.start();
     }
