513127e6c5256c869453bd0e96389c60b99715cc
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-07 22:27:15 -0300

    corrigir compilacao da protecao de colisao

--- src/main/java/com/sistemautil/PlayerCollisionListener.java
+++ src/main/java/com/sistemautil/PlayerCollisionListener.java
@@ -1,11 +1,9 @@
 package com.sistemautil;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityCollideWithEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.scoreboard.Scoreboard;
@@ -38,7 +36,7 @@ public final class PlayerCollisionListener implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
         disableCollision(event.getPlayer());
     }
@@ -46,14 +44,5 @@ public final class PlayerCollisionListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerMove(PlayerMoveEvent event) {
         disableCollision(event.getPlayer());
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerCollision(EntityCollideWithEntityEvent event) {
-        Entity first = event.getEntity();
-        Entity second = event.getOtherEntity();
-        if (first instanceof Player && second instanceof Player) {
-            event.setCancelled(true);
-        }
     }
 }
