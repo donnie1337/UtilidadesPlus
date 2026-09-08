4ca65d8e9862fb767c05767e8e885e9856d8c3ae
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-07 21:34:16 -0300

    otimizar controle de colisao entre jogadores

--- src/main/java/com/sistemautil/PlayerCollisionListener.java
+++ src/main/java/com/sistemautil/PlayerCollisionListener.java
@@ -5,17 +5,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public final class PlayerCollisionListener implements Listener {
 
     public void disableCollision(Player player) {
-        if (player != null && player.isOnline()) {
+        if (player != null && player.isOnline() && player.isCollidable()) {
             player.setCollidable(false);
         }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
+        disableCollision(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPlayerMove(PlayerMoveEvent event) {
         disableCollision(event.getPlayer());
     }
 }
