b2d5f2e2d2ce1909156a8af70d60a238d8fd6756
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-20 03:51:46 -0300

    fix(tab): center vanish using cargo and nickname width

--- src/main/java/com/sistemautil/tab/ServerTabManager.java
+++ src/main/java/com/sistemautil/tab/ServerTabManager.java
@@ -262,7 +262,7 @@ public final class ServerTabManager {
             return configuredSpaces + invisPart;
         }
 
-        String cleanName = plugin.getVisualText().format(namePart)
+        String cleanName = plugin.getVisualText().format((prefixPart == null ? "" : prefixPart) + (namePart == null ? "" : namePart))
                 .replaceAll("§[0-9A-FK-ORXx]", "")
                 .replaceAll("<[^>]+>", "");
         String cleanInvis = plugin.getVisualText().format(invisPart)
